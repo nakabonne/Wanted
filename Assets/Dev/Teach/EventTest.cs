8495cf80b0db288615598b762f7e667ffff7f2e8
佐藤太一 <Teach@satoutaichi-no-MacBook-Pro-2.local>
2016-12-03 03:34:56 +0900

    EventInvoker追加

---
+++ Assets/Dev/Teach/EventTest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour {
+
+	public void DebugTest(){
+		Debug.Log("aaaa");
+	}
+}
---
+++ Assets/EventInvoker/Scripts/Editor/EventInvokerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// イベントを実行するボタン作る
+/// </summary>
+[CustomEditor(typeof(EventInvoker))]
+public class EventInvokerEditor :  Editor{
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector ();
+		EventInvoker eventInvoker = (EventInvoker)target;
+		// ボタン作成
+		if (GUILayout.Button ("InvokeEvent")) {
+			eventInvoker.InvokeEvent ();
+		}
+	}
+}
