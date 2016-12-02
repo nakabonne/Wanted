60abef5d236e111af60b895232085f52c1382830
佐藤太一 <Teach@satoutaichi-no-MacBook-Pro-2.local>
2016-12-03 08:02:07 +0900

    Audio,Effect,ResourcesManager追加　召喚エフェクト追加

---
+++ Assets/Scripts/Manager/ResourcesManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResourcesManager : SingletonMonoBehaviour<ResourcesManager> {
+
+
+//	public readonly string hpBarPath = "Prefabs/UI/HPBar";
+//	private GameObject _hpBar;
+//	public GameObject HPBar{
+//		get{
+//			return _hpBar ??( _hpBar = LoadAsset<GameObject> (hpBarPath));
+//		}
+//	}
+//
+
+	public readonly string modelFolderPath = "Prefabs/Model/";
+
+	// Resourcesフォルダから指定したPathのAssetをロード
+	T LoadAsset<T>(string path) where T : Object
+	{
+		T asset = Resources.Load<T> (path);
+		if (!asset) {
+			Debug.LogError (path + "が取得できませんでした");
+		}
+		return asset;
+	}
+
+
+
+
+	/// <summary>
+	/// Resourcesからキャラクターのプレハブを取得
+	/// </summary>
+	/// <returns>The model.</returns>
+	/// <param name="charaID">Chara I.</param>
+	public GameObject GetModel(string charaID){
+		GameObject targetModel;
+		string modelPath = modelFolderPath + charaID;
+		targetModel = LoadAsset<GameObject> (modelPath);
+		return targetModel;
+	}
+
+	public AudioClip GetBGM(string path){
+		string audioPath = string.Format ("Audio/BGM/{0}", path);
+		AudioClip audio = GetAudio(audioPath);
+		return audio;
+	}
+
+	public AudioClip GetSE(string path){
+		string audioPath = string.Format ("Audio/SE/{0}", path);
+		AudioClip audio = GetAudio(audioPath);
+		return audio;
+	}
+		
+	AudioClip GetAudio(string path){
+		AudioClip audio;
+		audio = LoadAsset<AudioClip> (path);
+		return audio;
+	}
+
+	// Resourcesフォルダからエフェクトを取得
+	public GameObject GetEffect(string fileName){
+		GameObject go = LoadAsset<GameObject> ("Prefabs/Effects/" + fileName);
+		return go;
+	}
+}
