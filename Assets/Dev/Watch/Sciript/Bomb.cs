dcdfb860b7ac07fa478a78433a4100abff4b67ba
ryonakao <rodriguez.nak@gmail.com>
2016-12-03 16:55:11 +0900

    炎アセットの追加

--- Assets/Dev/Watch/Sciript/Bomb.cs
+++ Assets/Dev/Watch/Sciript/Bomb.cs
@@ -6,7 +6,7 @@ public class Bomb : MonoBehaviour {
 
 	public float stayTime = 1.0f;
 
-	public GameObject explosion;
+	public GameObject[] explosions = new GameObject[4];
 
 	Exposion exposion;
 	// Use this for initialization
@@ -71,50 +71,75 @@ public class Bomb : MonoBehaviour {
 			}
 		}
 
-		if (Physics.Raycast(rayright,out hitInfoRight,distance))
-		{
+		if (Physics.Raycast (rayright, out hitInfoRight, distance)) {
 			if (hitInfoRight.collider.tag == "Wall") {
 				Debug.Log ("右");
 				StartCoroutine (PutExplosion ("右"));
 			}
+		} else {
+			StartCoroutine (PutExplosion (" "));
 		}
 	}
 	//エフェクト生成
 	IEnumerator PutExplosion(string Walldirection){
 		yield return new WaitForSeconds(1.0f);
+		//四方向に生成
+		Instantiate (explosions[0], transform.position + transform.forward, Quaternion.identity);
+		Instantiate (explosions[1], transform.position - transform.forward, Quaternion.identity);
+		Instantiate (explosions[2], transform.position - transform.right , Quaternion.identity);
+		Instantiate (explosions[3], transform.position + transform.right, Quaternion.identity);
 
-		//四方にエフェクトを生成
-		for(int i=0; i<4; i++)
+		//爆風範囲内に障害物があった場合、その方向にあるexplosionの大きさを調整
+		switch (Walldirection) 
 		{
-			if (Walldirection == "前" && i == 0) {
-				Debug.Log ("前だよ");
-				//exposion.ScaleChange ("前");
-				//GameObject up = explosion;
-				//up.transform.localScale -= Vector3.forward;
-				//explosion.transform.localScale -= Vector3.forward;
-			}
-			if (Walldirection == "右" && i == 1) {
-				Debug.Log ("右だよ");
-				//exposion.ScaleChange ("右");
-				//GameObject right = explosion;
-				//explosion.transform.localScale -= Vector3.right;
-			}
-			if (Walldirection == "下" && i == 2) {
-				Debug.Log ("下だよ");
-				//exposion.ScaleChange ("下");
-				//GameObject up = explosion;
-				//GameObject down = explosion;
-				//explosion.transform.localScale -= new Vector3 (0,0,-1);
-			}
-			if (Walldirection == "左" && i == 3) {
-				Debug.Log ("左だよ");
-				//exposion.ScaleChange ("左");
-				//GameObject left = explosion;
-				//explosion.transform.localScale -= new Vector3 (-1, 0, 0);
-			}
-			Instantiate (explosion,transform.forward,Quaternion.identity);
-			transform.Rotate(new Vector3(0, 1, 0), 90);
+		case "前":
+			Debug.Log ("前のを小さくしたよ");
+			explosions [0].transform.localScale = new Vector3 (2.0f, 3.0f, 0.5f);
+			break;
+		case "下":
+			explosions [1].transform.localScale -= new Vector3 (0, 0, -1);
+			break;
+		case "左":
+			explosions [2].transform.localScale -= new Vector3 (1, 0, 0);
+			break;
+		case "右":
+			explosions [0].transform.localScale -= new Vector3 (-1, 0, 0);
+			break;
 		}
+
+
+//		//四方にエフェクトを生成
+//		for(int i=0; i<4; i++)
+//		{
+//			if (Walldirection == "前" && i == 0) {
+//				Debug.Log ("前だよ");
+//				//exposion.ScaleChange ("前");
+//				//GameObject up = explosion;
+//				//up.transform.localScale -= Vector3.forward;
+//				//explosion.transform.localScale -= Vector3.forward;
+//			}
+//			if (Walldirection == "右" && i == 1) {
+//				Debug.Log ("右だよ");
+//				//exposion.ScaleChange ("右");
+//				//GameObject right = explosion;
+//				//explosion.transform.localScale -= Vector3.right;
+//			}
+//			if (Walldirection == "下" && i == 2) {
+//				Debug.Log ("下だよ");
+//				//exposion.ScaleChange ("下");
+//				//GameObject up = explosion;
+//				//GameObject down = explosion;
+//				//explosion.transform.localScale -= new Vector3 (0,0,-1);
+//			}
+//			if (Walldirection == "左" && i == 3) {
+//				Debug.Log ("左だよ");
+//				//exposion.ScaleChange ("左");
+//				//GameObject left = explosion;
+//				//explosion.transform.localScale -= new Vector3 (-1, 0, 0);
+//			}
+//			Instantiate (explosion,transform.forward,Quaternion.identity);
+//			transform.Rotate(new Vector3(0, 1, 0), 90);
+//		}
 	}
 
 }
