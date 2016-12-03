9ec9c026806f4649983e9506ff4030f3359cef46
Rinaya <izy54.o@gmail.com>
2016-12-03 19:41:38 +0900

    result

--- Assets/Scripts/Scene/CharaSelect/CharacterSelectView.cs
+++ Assets/Scripts/Scene/CharaSelect/CharacterSelectView.cs
@@ -6,21 +6,27 @@ public class CharacterSelectView : MonoBehaviour {
 
 	[SerializeField]
 	private Transform[] showPos;
+	[SerializeField]
+	private GameObject[] label;
 
 	// プレイヤーを表示
 	public void ShowPlayer(int playerID){
 		string charaName = string.Empty;
 		switch (playerID) {
 		case 1:
+			label [playerID - 1].SetActive (true);
 			charaName = "Utc";
 			break;
 		case 2:
+			label [playerID - 1].SetActive (true);
 			charaName = "Misaki";
 			break;
 		case 3:
+			label [playerID - 1].SetActive (true);
 			charaName = "Yuko";
 			break;
 		case 4:
+			label [playerID - 1].SetActive (true);
 			// 4体目のプレハブを呼ぶ
 			break;
 		
