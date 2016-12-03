73129f35a7b1dbbbb2ed9e0e27b8d9eba5c78c1c
佐藤太一 <Teach@satoutaichi-no-MacBook-Pro-2.local>
2016-12-03 17:23:53 +0900

    キャラクター選択画面において，SDキャラが出るように変更(ただし，影がやばいのでシェーダー変更したほうがよい)

--- Assets/Scripts/Scene/CharaSelect/CharacterSelectView.cs
+++ Assets/Scripts/Scene/CharaSelect/CharacterSelectView.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class CharacterSelectView : MonoBehaviour {
 
-	public GameObject test;
-
 	[SerializeField]
 	private Transform[] showPos;
 
 	// プレイヤーを表示
 	public void ShowPlayer(int playerID){
-		Instantiate (test, showPos [playerID-1].position, Quaternion.identity);
+		string charaName = string.Empty;
+		switch (playerID) {
+		case 1:
+			charaName = "Utc";
+			break;
+		case 2:
+			charaName = "Misaki";
+			break;
+		case 3:
+			charaName = "Yuko";
+			break;
+		case 4:
+			// 4体目のプレハブを呼ぶ
+			break;
+		
+		}
+		GameObject obj = ResourcesManager.Instance.GetModel(charaName);
+		GameObject chara = Instantiate (obj, showPos [playerID-1].position, showPos[playerID-1].rotation) as GameObject;
+		chara.transform.localScale = Vector3.one * 4f;
+
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.A)) {
+			ShowPlayer (1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.B)) {
+			ShowPlayer (2);
+		}
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+			ShowPlayer (3);
+		}
 	}
 }
