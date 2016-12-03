718577e6ffe7f5d8c0a7c923f4d6ccdab5faecad
ryonakao <rodriguez.nak@gmail.com>
2016-12-03 10:37:38 +0900

    落下状態では操作不可

---
+++ Assets/Dev/Watch/Sciript/PlayerAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Beam ();
+		}
+		
+	}
+	//ビームを出す
+	void Beam()
+	{
+		Debug.Log ("ビーム");
+	}
+
+	void Bom()
+	{
+		
+	}
+}
