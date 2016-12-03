19d7d538d28d3727e230f9a237547761fd217b3d
Rinaya <izy54.o@gmail.com>
2016-12-03 20:43:44 +0900

    Result

---
+++ Assets/Dev/ayarin/Result.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+
+	public Text result;
+
+	// Use this for initialization
+	void Start () {
+		result.text = "1. " + ScoreManager.Instance.ranking[0] + "P\n"
+			+ "2. " + ScoreManager.Instance.ranking[1] + "P\n"
+			+ "3. " + ScoreManager.Instance.ranking[2] + "P\n"
+			+ "4. " + ScoreManager.Instance.ranking[3] + "P\n";
+	}
+
+}
---
+++ Assets/Dev/ayarin/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : SingletonMonoBehaviour<ScoreManager> {
+
+	public int[] ranking = new int[4];
+	int top = 0;
+
+	public void SetRanking(int playerID){
+		ranking [top] = playerID;
+		top++;
+	}
+}
